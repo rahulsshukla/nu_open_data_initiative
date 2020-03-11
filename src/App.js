b5ea0aa8cd1655812dce180a7bcf818596d14e41
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-03-11 17:05:32 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+//component imports
+import MainPage from "../src/components/MainPage";
+
+//firebase imports
+
+//react + css
+
+//router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/MainPage.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+
+const MainPage = () => {
+  //states and such here
+  return <p style={{ margin: "0 2em 0 0" }}>NODSSSSS</p>;
+};
+
+export default MainPage;
