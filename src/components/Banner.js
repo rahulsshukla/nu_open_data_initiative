b8fa7fc6d364d25355e8b605131c5854ced85cdc
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-03-13 14:19:59 -0500

    logo present

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from "react";
 import "semantic-ui-css/semantic.min.css";
 import "../styles/Banner.css";
+import logo from "../NODI.png";
 const Banner = () => {
   //states and such here
   return (
-    <h1 id="nodi" class="ui header">
-      northwestern open data initiative{" "}
-    </h1>
+    <img
+      src={logo}
+      alt="Northwestern Open Data POrtal"
+      height="200"
+      width="200"
+    />
   );
 };
 
