43018339859a6f07720de62238ae065d00e6c478
alexr17 <alexanderrhee2021@u.northwestern.edu>
2020-05-31 14:06:47 -0700

    trying more random shit out

--- nodi_backend/settings/base.py
+++ nodi_backend/settings/base.py
@@ -71,17 +71,11 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': (
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ),
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #     'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    #     'rest_framework.authentication.BasicAuthentication',
-    # ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (),
+    'DEFAULT_PERMISSION_CLASSES': (),
 }
 
 WSGI_APPLICATION = 'nodi_backend.wsgi.application'
--- nodi_backend/urls.py
+++ nodi_backend/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls import url
 
 urlpatterns = [
-    # path('', include('datasets.urls')),
+    path('', include('datasets.urls')),
     re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
     
 ]
