3d8edb91eea6482e14d2e83b787368620ea6fb92
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-06-03 20:08:45 -0500

    error handling done for upload form

--- src/data/client.js
+++ src/data/client.js
@@ -1,11 +1,19 @@
 export const getCategories = async (callback) => {
   const response = await fetch('https://nodi-backend.herokuapp.com/api/categories')
   const data = await response.json();
+  console.log(data)
   return callback(data);
 }
 
 export const getDataTypes = async (callback) => {
   const response = await fetch('https://nodi-backend.herokuapp.com/api/datatypes')
   const data = await response.json();
+  console.log(data)
   return callback(data);
 }
+
+export const getDatasets = async (callback) => {
+  const response = await fetch('https://nodi-backend.herokuapp.com/api/datatypes')
+  const data = await response.json();
+  console.log(data);
+};
