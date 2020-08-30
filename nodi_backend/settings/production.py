360fa953638893d58f3bae762571054735f959a3
alexr17 <alexanderrhee2021@u.northwestern.edu>
2020-08-30 16:43:46 -0700

    add nodi.nu as domain

--- nodi_backend/settings/production.py
+++ nodi_backend/settings/production.py
@@ -3,7 +3,7 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nodi-backend.herokuapp.com']
+ALLOWED_HOSTS = ['nodi-backend.herokuapp.com', 'nodi.nu']
 
 SECURE_SSL_REDIRECT = True
 
