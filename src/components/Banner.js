3635b82729193c340731d16170b2b000efded9a2
aldo <aldoaguilar2022@u.northwestern.edu>
2021-11-14 15:31:23 -0600

    [#60] Updated Banner.js to be more modular

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,10 +7,21 @@ import { AppState } from "../data/context";
 import RelatedLinks from './RelatedLinks.js';
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
+
+
 const Banner = () => {
   const state = useContext(AppState);
   const { clearQueries } = state;
 
+  function bannerButton(linkTo, buttonName) {
+    return(
+    <Link to={linkTo} onClick={() => clearQueries(false)}>
+      <Button id="button">
+        {buttonName}
+      </Button>
+    </Link>)
+  }
+  
   function mobileNav(){
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -20,31 +31,19 @@ const Banner = () => {
       x.className = "topnav";
     }
   }
+
   return (
    <div className="banner">
-      <Link to="/" onClick={() => clearQueries(false)}>
+      <Link to="/Home" onClick={() => clearQueries(false)}>
         <img src={logo} alt="Northwestern Open Data Portal" width="180px" className="logonav"/>
       </Link>
       <span className="ico" name="menu" onClick={mobileNav}>&#9776;</span>
+
       <div className="topnav" id="myTopnav"> 
-        {/* 
-        <Link to="/datasets" onClick={() => clearQueries(true)}>
-          <Button id="button">
-            Datasets
-          </Button>
-        </Link>
-        */}
-        <Link to="/contribute" onClick={() => clearQueries(false)}>
-          <Button id="button">
-            Contribute a Dataset
-          </Button>
-        </Link>
-        <Link to="/aboutus" onClick={() => clearQueries(false)}>
-          <Button id="button">
-            About Us
-          </Button>
-        </Link>
-        <RelatedLinks />
+        {bannerButton('/datasets', 'Datasets')}
+        {bannerButton("/contribute", "Contribute A Dataset")}
+        {bannerButton("/aboutus", "About Us")}
+        <RelatedLinks/>
       </div>
   </div>
   );
